sydhds/polkadot-sdk

Add RISC-V functionality to pallet-contracts

substrate/client/executor/wasmtime/src/host.rs
@@ -19,12 +19,15 @@
 //! This module defines `HostState` and `HostContext` structs which provide logic and state
 //! required for execution of host.
 
-use wasmtime::Caller;
-
-use sc_allocator::{AllocationStats, FreeingBumpHeapAllocator};
-use sp_wasm_interface::{Pointer, WordSize};
-
 use crate::{instance_wrapper::MemoryWrapper, runtime::StoreData, util};
+use sc_allocator::{AllocationStats, FreeingBumpHeapAllocator};
+use sp_virtualization::{
+	DestroyError as VirtDestroyError, ExecError as VirtExecError, Memory as VirtMemory, MemoryT,
+	SharedState as VirtSharedState, Virt, VirtT,
+};
+use sp_wasm_interface::{Pointer, WordSize};
+use std::{collections::HashMap, mem};
+use wasmtime::{AsContext, Caller, TypedFunc};
 
 /// The state required to construct a HostContext context. The context only lasts for one host
 /// call, whereas the state is maintained for the duration of a Wasm runtime call, which may make
@@ -37,12 +40,19 @@ pub struct HostState {
 	/// once.
 	allocator: Option<FreeingBumpHeapAllocator>,
 	panic_message: Option<String>,
+	virt_instances: HashMap<u64, VirtOrMem>,
+	virt_counter: u64,
 }
 
 impl HostState {
 	/// Constructs a new `HostState`.
 	pub fn new(allocator: FreeingBumpHeapAllocator) -> Self {
-		HostState { allocator: Some(allocator), panic_message: None }
+		HostState {
+			allocator: Some(allocator),
+			panic_message: None,
+			virt_instances: Default::default(),
+			virt_counter: 0,
+		}
 	}
 
 	/// Takes the error message out of the host state, leaving a `None` in its place.
@@ -124,5 +134,217 @@ impl<'a> sp_wasm_interface::FunctionContext for HostContext<'a> {
 
 	fn register_panic_error_message(&mut self, message: &str) {
 		self.host_state_mut().panic_message = Some(message.to_owned());
+	}
+
+	fn virtualization(&mut self) -> &mut dyn sp_wasm_interface::Virtualization {
+		self
+	}
+}
+
+impl<'a> sp_wasm_interface::Virtualization for HostContext<'a> {
+	fn instantiate(&mut self, program: &[u8]) -> sp_wasm_interface::Result<Result<u64, u8>> {
+		let virt = match Virt::instantiate(program) {
+			Ok(virt) => virt,
+			Err(err) => return Ok(Err(err.into())),
+		};
+
+		let host = self.host_state_mut();
+
+		let instance_id = {
+			let old = host.virt_counter;
+			host.virt_counter = old + 1;
+			old
+		};
+
+		host.virt_instances.insert(instance_id, VirtOrMem::Instance(virt));
+
+		Ok(Ok(instance_id))
+	}
+
+	fn execute(
+		&mut self,
+		instance_id: u64,
+		function: &str,
+		syscall_handler: u32,
+		state_ptr: u32,
+		destroy: bool,
+	) -> sp_wasm_interface::Result<Result<(), u8>> {
+		let mut virt = match self.host_state_mut().virt_instances.remove(&instance_id) {
+			Some(VirtOrMem::Instance(virt)) => virt,
+			Some(VirtOrMem::Memory(_)) =>
+				Err("it is illegal to call execute while already executing")?,
+			None => return Ok(Err(VirtExecError::InvalidInstance.into())),
+		};
+
+		// Extract a syscall handler from the instance's table by the specified index.
+		let syscall_handler = {
+			let table = self
+				.caller
+				.data()
+				.table
+				.ok_or("Runtime doesn't have a table; sandbox is unavailable")?;
+			let table_item = table.get(&mut self.caller, syscall_handler);
+
+			table_item
+				.ok_or("dispatch_thunk_id is out of bounds")?
+				.funcref()
+				.ok_or("dispatch_thunk_idx should be a funcref")?
+				.ok_or("dispatch_thunk_idx should point to actual func")?
+				.typed(&mut self.caller)
+				.map_err(|_| "dispatch_thunk_idx has the wrong type")?
+		};
+
+		self.host_state_mut()
+			.virt_instances
+			.insert(instance_id, VirtOrMem::Memory(virt.memory()));
+
+		let mut shared_state = VirtSharedState {
+			gas_left: 0,
+			exit: false,
+			user: VirtContext { host: self, syscall_handler, state_ptr },
+		};
+
+		let outcome = virt.execute(function, virt_syscall_handler, &mut shared_state);
+
+		if destroy {
+			self.host_state_mut().virt_instances.remove(&instance_id);
+		} else {
+			self.host_state_mut()
+				.virt_instances
+				.insert(instance_id, VirtOrMem::Instance(virt));
+		}
+
+		Ok(outcome.map_err(Into::into))
+	}
+
+	fn destroy(&mut self, instance_id: u64) -> sp_wasm_interface::Result<Result<(), u8>> {
+		if self.host_state_mut().virt_instances.remove(&instance_id).is_some() {
+			Ok(Ok(()))
+		} else {
+			Ok(Err(VirtDestroyError::InvalidInstance.into()))
+		}
+	}
+
+	fn read_memory(
+		&mut self,
+		instance_id: u64,
+		offset: u32,
+		buf_ptr: u32,
+		buf_len: u32,
+	) -> sp_wasm_interface::Result<Result<(), u8>> {
+		let Some(memory) = self
+			.host_state_mut()
+			.virt_instances
+			.get(&instance_id)
+			.map(|instance| instance.memory())
+		else {
+			return Ok(Err(VirtDestroyError::InvalidInstance.into()))
+		};
+		let mut buf = vec![0u8; buf_len as usize];
+		if let Err(err) = memory.read(offset, buf.as_mut()) {
+			return Ok(Err(err.into()))
+		}
+		util::write_memory_from(&mut self.caller, buf_ptr.into(), buf.as_ref())
+			.map_err(|_| "Failed to write memory from the virt instance to the supervisor")?;
+		Ok(Ok(()))
+	}
+
+	fn write_memory(
+		&mut self,
+		instance_id: u64,
+		offset: u32,
+		buf_ptr: u32,
+		buf_len: u32,
+	) -> sp_wasm_interface::Result<Result<(), u8>> {
+		let Some(mut memory) = self
+			.host_state_mut()
+			.virt_instances
+			.get(&instance_id)
+			.map(|instance| instance.memory())
+		else {
+			return Ok(Err(VirtDestroyError::InvalidInstance.into()))
+		};
+		let mut buf = vec![0u8; buf_len as usize];
+		util::read_memory_into(&self.caller, buf_ptr.into(), buf.as_mut())
+			.map_err(|_| "Failed to read memory from supervisor")?;
+		if let Err(err) = memory.write(offset, buf.as_ref()) {
+			return Ok(Err(err.into()))
+		}
+		Ok(Ok(()))
+	}
+}
+
+enum VirtOrMem {
+	Instance(Virt),
+	Memory(VirtMemory),
+}
+
+impl VirtOrMem {
+	fn memory(&self) -> VirtMemory {
+		match self {
+			Self::Instance(virt) => virt.memory(),
+			Self::Memory(memory) => memory.clone(),
+		}
+	}
+}
+
+/// Data structure that is passed into our registered callback.
+struct VirtContext<'a, 'b> {
+	/// Needed to get a wasm handler so we can call our `syscall_hander`
+	host: &'a mut HostContext<'b>,
+	/// Wasm function we call to handle our syscall.
+	syscall_handler: TypedFunc<(u32, u32, u32, u32, u32, u32, u32, u32), u64>,
+	/// First argument to the `syscall_handler`.
+	state_ptr: u32,
+}
+
+impl<'a, 'b> VirtContext<'a, 'b> {
+	fn state_in_wasm(&mut self) -> Option<&mut VirtSharedState<()>> {
+		let offset = self.state_ptr as usize;
+		let buf = self.host.caller.as_context().data().memory().data_mut(&mut self.host.caller);
+		let scoped =
+			buf.get_mut(offset..offset.saturating_add(mem::size_of::<VirtSharedState<()>>()))?;
+		unsafe { Some(&mut *(scoped.as_mut_ptr() as *mut _)) }
+	}
+}
+
+extern "C" fn virt_syscall_handler(
+	state: &mut VirtSharedState<VirtContext<'_, '_>>,
+	syscall_no: u32,
+	a0: u32,
+	a1: u32,
+	a2: u32,
+	a3: u32,
+	a4: u32,
+	a5: u32,
+) -> u64 {
+	let syscall_handler = state.user.syscall_handler;
+	let state_ptr = state.user.state_ptr;
+
+	// sync current gas counter to wasm
+	if let Some(shared_state) = state.user.state_in_wasm() {
+		shared_state.gas_left = state.gas_left;
+	} else {
+		// error out early of out of bound pointer was supplied
+		state.exit = true;
+		return u64::MAX
+	}
+
+	let result = syscall_handler
+		.call(&mut state.user.host.caller, (state_ptr, syscall_no, a0, a1, a2, a3, a4, a5));
+	match result {
+		Ok(outcome) => {
+			// those fields could have been changes by handler: copy back from wasm
+			let shared_state = state.user.state_in_wasm().expect("was checked above; qed");
+			state.gas_left = shared_state.gas_left;
+			state.exit = shared_state.exit;
+			outcome
+		},
+		Err(err) => {
+			log::error!("RiscV syscall handler failed: {}", err);
+			// we trap the execution. return value is not used in this case.
+			state.exit = true;
+			u64::MAX
+		},
 	}
 }

paritytech-secops/polkadot-sdk-test

substrate/client/executor/wasmtime/src/host.rs
@@ -19,12 +19,15 @@
 //! This module defines `HostState` and `HostContext` structs which provide logic and state
 //! required for execution of host.
 
-use wasmtime::Caller;
-
-use sc_allocator::{AllocationStats, FreeingBumpHeapAllocator};
-use sp_wasm_interface::{Pointer, WordSize};
-
 use crate::{instance_wrapper::MemoryWrapper, runtime::StoreData, util};
+use sc_allocator::{AllocationStats, FreeingBumpHeapAllocator};
+use sp_virtualization::{
+	DestroyError as VirtDestroyError, ExecError as VirtExecError, Memory as VirtMemory, MemoryT,
+	SharedState as VirtSharedState, Virt, VirtT,
+};
+use sp_wasm_interface::{Pointer, WordSize};
+use std::{collections::HashMap, mem};
+use wasmtime::{AsContext, Caller, TypedFunc};
 
 /// The state required to construct a HostContext context. The context only lasts for one host
 /// call, whereas the state is maintained for the duration of a Wasm runtime call, which may make
@@ -37,12 +40,19 @@ pub struct HostState {
 	/// once.
 	allocator: Option<FreeingBumpHeapAllocator>,
 	panic_message: Option<String>,
+	virt_instances: HashMap<u64, VirtOrMem>,
+	virt_counter: u64,
 }
 
 impl HostState {
 	/// Constructs a new `HostState`.
 	pub fn new(allocator: FreeingBumpHeapAllocator) -> Self {
-		HostState { allocator: Some(allocator), panic_message: None }
+		HostState {
+			allocator: Some(allocator),
+			panic_message: None,
+			virt_instances: Default::default(),
+			virt_counter: 0,
+		}
 	}
 
 	/// Takes the error message out of the host state, leaving a `None` in its place.
@@ -124,5 +134,209 @@ impl<'a> sp_wasm_interface::FunctionContext for HostContext<'a> {
 
 	fn register_panic_error_message(&mut self, message: &str) {
 		self.host_state_mut().panic_message = Some(message.to_owned());
+	}
+
+	fn virtualization(&mut self) -> &mut dyn sp_wasm_interface::Virtualization {
+		self
+	}
+}
+
+impl<'a> sp_wasm_interface::Virtualization for HostContext<'a> {
+	fn instantiate(&mut self, program: &[u8]) -> sp_wasm_interface::Result<Result<u64, u8>> {
+		let virt = match Virt::instantiate(program) {
+			Ok(virt) => virt,
+			Err(err) => return Ok(Err(err.into())),
+		};
+
+		let host = self.host_state_mut();
+
+		let instance_id = {
+			let old = host.virt_counter;
+			host.virt_counter = old + 1;
+			old
+		};
+
+		host.virt_instances.insert(instance_id, VirtOrMem::Instance(virt));
+
+		Ok(Ok(instance_id))
+	}
+
+	fn execute(
+		&mut self,
+		instance_id: u64,
+		function: &str,
+		syscall_handler: u32,
+		state_ptr: u32,
+		destroy: bool,
+	) -> sp_wasm_interface::Result<Result<(), u8>> {
+		let mut virt = match self.host_state_mut().virt_instances.remove(&instance_id) {
+			Some(VirtOrMem::Instance(virt)) => virt,
+			Some(VirtOrMem::Memory(_)) =>
+				Err("it is illegal to call execute while already executing")?,
+			None => return Ok(Err(VirtExecError::InvalidInstance.into())),
+		};
+
+		// Extract a syscall handler from the instance's table by the specified index.
+		let syscall_handler = {
+			let table = self
+				.caller
+				.data()
+				.table
+				.ok_or("Runtime doesn't have a table; sandbox is unavailable")?;
+			let table_item = table.get(&mut self.caller, syscall_handler);
+
+			table_item
+				.ok_or("dispatch_thunk_id is out of bounds")?
+				.funcref()
+				.ok_or("dispatch_thunk_idx should be a funcref")?
+				.ok_or("dispatch_thunk_idx should point to actual func")?
+				.typed(&mut self.caller)
+				.map_err(|_| "dispatch_thunk_idx has the wrong type")?
+		};
+
+		self.host_state_mut()
+			.virt_instances
+			.insert(instance_id, VirtOrMem::Memory(virt.memory()));
+
+		let mut shared_state = VirtSharedState {
+			gas_left: 0,
+			exit: false,
+			user: VirtContext { host: self, syscall_handler, state_ptr },
+		};
+
+		let outcome = virt.execute(function, virt_syscall_handler, &mut shared_state);
+
+		if destroy {
+			self.host_state_mut().virt_instances.remove(&instance_id);
+		} else {
+			self.host_state_mut()
+				.virt_instances
+				.insert(instance_id, VirtOrMem::Instance(virt));
+		}
+
+		Ok(outcome.map_err(Into::into))
+	}
+
+	fn destroy(&mut self, instance_id: u64) -> sp_wasm_interface::Result<Result<(), u8>> {
+		if self.host_state_mut().virt_instances.remove(&instance_id).is_some() {
+			Ok(Ok(()))
+		} else {
+			Ok(Err(VirtDestroyError::InvalidInstance.into()))
+		}
+	}
+
+	fn read_memory(
+		&mut self,
+		instance_id: u64,
+		offset: u32,
+		dest: &mut [u8],
+	) -> sp_wasm_interface::Result<Result<(), u8>> {
+		let Some(memory) = self
+			.host_state_mut()
+			.virt_instances
+			.get(&instance_id)
+			.map(|instance| instance.memory())
+		else {
+			return Ok(Err(VirtDestroyError::InvalidInstance.into()))
+		};
+		if let Err(err) = memory.read(offset, dest) {
+			return Ok(Err(err.into()))
+		}
+		Ok(Ok(()))
+	}
+
+	fn write_memory(
+		&mut self,
+		instance_id: u64,
+		offset: u32,
+		src: &[u8],
+	) -> sp_wasm_interface::Result<Result<(), u8>> {
+		let Some(mut memory) = self
+			.host_state_mut()
+			.virt_instances
+			.get(&instance_id)
+			.map(|instance| instance.memory())
+		else {
+			return Ok(Err(VirtDestroyError::InvalidInstance.into()))
+		};
+		if let Err(err) = memory.write(offset, src) {
+			return Ok(Err(err.into()))
+		}
+		Ok(Ok(()))
+	}
+}
+
+enum VirtOrMem {
+	Instance(Virt),
+	Memory(VirtMemory),
+}
+
+impl VirtOrMem {
+	fn memory(&self) -> VirtMemory {
+		match self {
+			Self::Instance(virt) => virt.memory(),
+			Self::Memory(memory) => memory.clone(),
+		}
+	}
+}
+
+/// Data structure that is passed into our registered callback.
+struct VirtContext<'a, 'b> {
+	/// Needed to get a wasm handler so we can call our `syscall_hander`
+	host: &'a mut HostContext<'b>,
+	/// Wasm function we call to handle our syscall.
+	syscall_handler: TypedFunc<(u32, u32, u32, u32, u32, u32, u32, u32), u64>,
+	/// First argument to the `syscall_handler`.
+	state_ptr: u32,
+}
+
+impl<'a, 'b> VirtContext<'a, 'b> {
+	fn state_in_wasm(&mut self) -> Option<&mut VirtSharedState<()>> {
+		let offset = self.state_ptr as usize;
+		let buf = self.host.caller.as_context().data().memory().data_mut(&mut self.host.caller);
+		let scoped =
+			buf.get_mut(offset..offset.saturating_add(mem::size_of::<VirtSharedState<()>>()))?;
+		unsafe { Some(&mut *(scoped.as_mut_ptr() as *mut _)) }
+	}
+}
+
+extern "C" fn virt_syscall_handler(
+	state: &mut VirtSharedState<VirtContext<'_, '_>>,
+	syscall_no: u32,
+	a0: u32,
+	a1: u32,
+	a2: u32,
+	a3: u32,
+	a4: u32,
+	a5: u32,
+) -> u64 {
+	let syscall_handler = state.user.syscall_handler;
+	let state_ptr = state.user.state_ptr;
+
+	// sync current gas counter to wasm
+	if let Some(shared_state) = state.user.state_in_wasm() {
+		shared_state.gas_left = state.gas_left;
+	} else {
+		// error out early of out of bound pointer was supplied
+		state.exit = true;
+		return u64::MAX
+	}
+
+	let result = syscall_handler
+		.call(&mut state.user.host.caller, (state_ptr, syscall_no, a0, a1, a2, a3, a4, a5));
+	match result {
+		Ok(outcome) => {
+			// those fields could have been changes by handler: copy back from wasm
+			let shared_state = state.user.state_in_wasm().expect("was checked above; qed");
+			state.gas_left = shared_state.gas_left;
+			state.exit = shared_state.exit;
+			outcome
+		},
+		Err(err) => {
+			log::error!("virtualization syscall handler failed: {}", err);
+			// we trap the execution. return value is not used in this case.
+			state.exit = true;
+			u64::MAX
+		},
 	}
 }